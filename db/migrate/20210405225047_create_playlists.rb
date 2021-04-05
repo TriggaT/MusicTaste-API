b2780c72610e06d16d2bbe39e3bd95324efa1241
Trayshawn <traytwebb14@yahoo.com>
2021-04-05 17:52:46 -0500

    put playlist migration file before song migration file

--- db/migrate/20210405225047_create_playlists.rb
+++ db/migrate/20210405225047_create_playlists.rb
@@ -2,7 +2,7 @@ class CreatePlaylists < ActiveRecord::Migration[6.1]
   def change
     create_table :playlists do |t|
       t.string :name
-      t.text :description
+      t.string :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_02_194839) do
+ActiveRecord::Schema.define(version: 2021_04_05_225112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "songs", force: :cascade do |t|
-    t.string "title"
-    t.string "artist"
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.string "artist"
+    t.bigint "playlist_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["playlist_id"], name: "index_songs_on_playlist_id"
+  end
+
+  add_foreign_key "songs", "playlists"
 end
