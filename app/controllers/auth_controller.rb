abcfb04f5bab586f2b97fa15decd7eb4980c2975
Trayshawn <traytwebb14@yahoo.com>
2021-04-07 17:27:41 -0500

    added before action with require_login method and created said method along with logged_in? method

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,15 @@
 class AuthController < ApplicationController
-    
+    before_action :require_login
 
-  def login
+    def logged_in? 
+        !!session_user
+    end 
+
+    def require_login
+        render json: {message: "Please Login"}, status: :unauthorized unless logged_in? 
+    end 
+    
+    def login
       user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:user][:password])
         payload = {user_id: user.id}
@@ -10,15 +18,15 @@ class AuthController < ApplicationController
       else
         render json: {failure: "Your username or password is incorrect"}
       end  
-  end
+    end
 
-  def auto_login
+    def auto_login
         if session_user
             render json: session_user
         else 
             render json: {errors: "No user is logged in. Make sure you're logged in."}
         end  
-  end 
+    end 
   
   
 
