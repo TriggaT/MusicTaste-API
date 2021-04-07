26678b8279b7a294e139ba9e2415693ad2f7c598
Trayshawn <traytwebb14@yahoo.com>
2021-04-06 23:16:36 -0500

    added playlist_id in permitted params for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -46,6 +46,6 @@ class ReviewsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def review_params
-      params.require(:review).permit(:rating, :text)
+      params.require(:review).permit(:rating, :text, :playlist_id)
     end
 end
