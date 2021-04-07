1d27eafc396ffaaf879ac7eb7ae1f9dcc60ced8a
Trayshawn <traytwebb14@yahoo.com>
2021-04-07 16:40:27 -0500

    added session user method to applicatoin controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::API
     def encode_token(payload)
         JWT.encode(payload, ENV['MY_SECRET'])
     end 
+
+    def session_user
+        decoded_hash = decoded_token
+        if !decoded_hash.empty?
+            user_id =decoded_hash[0]["user_id"]
+            @user = User.find_by[id: user_id]
+        else 
+            nil
+        end 
+    end 
+
 end
