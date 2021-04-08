85e0950966dc4d1734ef0d50f4a5866cffe73260
Trayshawn <traytwebb14@yahoo.com>
2021-04-08 13:28:24 -0500

    changed password_digest into password and removed binding prys from auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,8 +6,7 @@ class AuthController < ApplicationController
     def login
         user = User.find_by(username: params[:username])
         
-        if user && user.authenticate(params[:password_digest])
-            binding.pry 
+        if user && user.authenticate(params[:password])
             payload = {user_id: user.id}
             token = encode_token(payload)
             render json: {user: user, jwt: token, success: "Welcome back, #{user.username}"}
