1640c74d20332b607c7535125343351ac8e6c45e
Trayshawn <traytwebb14@yahoo.com>
2021-04-08 15:35:04 -0500

    rendered instance_to_json upon creating review in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,7 +18,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
 
     if @review.save
-      render json: @review, status: :created, location: @review
+      render json: @review.instance_to_json, status: :created, location: @review
     else
       render json: @review.errors, status: :unprocessable_entity
     end
