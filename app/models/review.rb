89ca0c999f9334454c62046d4245076dca151b9f
Trayshawn <traytwebb14@yahoo.com>
2021-04-08 10:25:48 -0500

    create instace_to_json and array_to_json methods for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review < ApplicationRecord
     belongs_to :playlist
     belongs_to :user 
+
+    def self.array_to_json 
+        self.all.map do |p|
+            p.instance_to_json 
+        end 
+    end 
+
+    def instance_to_json
+        {
+            id: self.id,
+            rating: self.rating, 
+            text: self.text,
+            user: {
+                id: self.user.id,
+                name: self.user.username
+            }           
+        }
+    end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,22 @@ class User < ApplicationRecord
     has_secure_password
     has_many :playlist
     has_many :reviews
+
+    def self.array_to_json 
+        self.all.map do |p|
+            p.instance_to_json 
+        end 
+    end 
+
+    def instance_to_json
+        {
+            id: self.id,
+            name: self.name, 
+            description: self.description,
+            user: {
+                id: self.user.id,
+                name: self.user.username
+            }           
+        }
+    end 
 end
