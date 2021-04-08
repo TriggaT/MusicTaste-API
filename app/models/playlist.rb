e1cc5a3aebaf0975b62ce3973e7ce42a901782d1
Trayshawn <traytwebb14@yahoo.com>
2021-04-08 10:05:47 -0500

    create instace_to_json and array_to_json methods for playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,23 @@ class Playlist < ApplicationRecord
     has_many :songs
     has_many :reviews
     belongs_to :user
+
+    def self.array_to_json 
+        self.all.map do |p|
+            p.instance_to_json 
+        end 
+    end 
+
+    def instance_to_json
+        {
+            id: self.id,
+            name: self.name, 
+            description: self.price,
+            user: {
+                id: self.user.id,
+                name: self.user.name
+            }           
+        }
+    end 
+
 end
