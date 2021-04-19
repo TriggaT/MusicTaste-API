7e4ee0848e44b0b736d55b1bb6f75d33fbc668c5
Trayshawn <traytwebb14@yahoo.com>
2021-04-18 20:19:14 -0500

    removed unneeded route and method inside auth_controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -27,10 +27,6 @@ class AuthController < ApplicationController
         !!session_user
     end 
 
-    def require_login
-        render json: {message: "Please Login"}, status: :unauthorized unless logged_in? 
-    end 
-  
   
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resource :users, only: [:create]
    post "/login", to: "auth#login"
    get "/auto_login", to: "auth#auto_login"
-   get "/user_is_authed", to: "auth#user_is_authed"
   resources :reviews
   resources :playlists
   resources :songs
