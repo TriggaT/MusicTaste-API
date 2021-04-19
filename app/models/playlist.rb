1d023eb580979c0d928fcba395f01d23a4dcec82
Trayshawn <traytwebb14@yahoo.com>
2021-04-18 22:08:20 -0500

    playlists validates unique name

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,6 +2,7 @@ class Playlist < ApplicationRecord
     has_many :songs
     has_many :reviews
     belongs_to :user
+    validates :name, uniqueness: true
 
     def self.array_to_json 
         self.all.map do |p|
