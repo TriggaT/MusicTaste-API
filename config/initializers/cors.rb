c1bb23fb36c9b981ccec966a7d62f7a17f0d48a8
Trayshawn <traytwebb14@yahoo.com>
2021-05-07 16:59:23 -0500

    removed forward slash from musictaste front-end address

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://musictaste.netlify.app/"
+    origins "https://musictaste.netlify.app"
 
     resource '*',
       headers: :any,
