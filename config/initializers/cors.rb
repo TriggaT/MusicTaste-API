4f2ef5cb80ea0eb6dae256fc8741915976fd7539
Trayshawn <traytwebb14@yahoo.com>
2021-05-07 16:52:28 -0500

    properly added musictaste netlify address to cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,15 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000/', "https://musictaste.netlify.app/"
+    origins 'localhost:3000/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins "https://musictaste.netlify.app/"
 
     resource '*',
       headers: :any,
